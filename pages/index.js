61831bf40456b4d9acf7e42a6a321df9cbd18513
Fernando Amezcua <Fernando.Amezcua@syngenta.com>
2022-01-21 00:26:35 -0600

    Add piloto project

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,5 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Home from '../components/Home';
 
-export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          This is an example
-        </p>
-      </main>
-
-      <Footer />
-    </div>
-  )
+export default function Index() {
+  return <Home />
 }
